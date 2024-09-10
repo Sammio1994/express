870049f0f357c3de487af874c5936e753ff02266
Samm Wiltshire <samm94tw@gmail.com>
2024-09-10 11:59:05 +0100

    clean up for mongoose

--- src/server.js
+++ src/server.js
@@ -4,46 +4,25 @@ const app = express();
 
 app.use(express.json());
 
-const fakeDB = [];
 
-app.get("/books/onebook", (request, response) => {
-    const book = {
-        title:"book1",
-        author: "harry",
-        genra: "sports",
-    }
-    response.send({message: "success", book: book});
-});
 
 app.get("/books/fakeDB", (request, response) => {
 
-    response.send({message: "success", books: fakeDB});
+    response.send({message: "success" });
 });
 
 app.get("/books/allbooks" , (request, response) => {
-    response.send("hello all books");
+    response.send( "success" );
 }) 
 
 app.post("/books", (request, response) => {
-    fakeDB.push(request.body);
-    response.send({message: `${request.body.title} has been added` , book:request.body});
+    response.send({message:  "success"});
 }
 
 )
 
-app.get("/movies/OneMovie", (request, response) => {
-    
-    response.send("One Movie");
-});
-
-app.put("/movies/updatemovie", (request, response) => {
-    response.send("update movie");
-});
-
 app.listen(5000, () => {
     console.log("Server is listening on port 5000")
 });
-
-
 
 // import express from "express";
