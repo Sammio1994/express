d901b2716a7356be36262b8e603e692d5124bcdf
Samm Wiltshire <samm94tw@gmail.com>
2024-09-12 12:07:53 +0100

    refactor post route addbook

--- src/server.js
+++ src/server.js
@@ -6,26 +6,17 @@ const connection = require("./db/connection");
 
 const Book = require("./books/model");
 
+const bookRouter = require("./books/routes")
+
 const app = express();
 
 app.use(express.json());
 
 connection();
 
+app.use(bookRouter);
+
 //routes
-// post
-
-app.post("/books/addbook", async (request, response) => {
-    console.log("request.body: ", request.body.genre );
-
-    const book = await Book.create({
-        title: request.body.title,
-        author: request.body.author,
-        genre: request.body.genre,
-    });
-
-    response.send({ message:  "success", book: book });
-})
 
 // get all the books
 
